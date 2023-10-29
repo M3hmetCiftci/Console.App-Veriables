9a1ba6f878a4d043bdc6436d9619ba69fcaa2bfb
M3hmetCiftci <Mehmet.cift27@gmail.com>
2023-10-29 23:31:34 +0300

    Console.App-Variables

---
+++ Program.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Char Veriables ------------------------------------------------------------------------->
+
+            //char veriables;
+            //veriables= 'A';
+            //Console.WriteLine(veriables);
+            //Console.Read();
+
+
+            //char veriables;
+            //veriables = '+';
+            //Console.WriteLine(veriables);
+            //Console.Read();
+
+
+            //char selection;
+            //Console.WriteLine("Selection: A B C");
+            //selection=Convert.ToChar(Console.ReadLine());
+            //Console.Write("selection: "+selection);
+            //Console.Read();
+
+            //Byte Veriables ------------------------------------------------------------------------->
+
+            //// 0 - 255 
+            //byte Number;
+            //Number = 255;        
+            //Console.WriteLine(Number);
+            //Console.Read();
+
+
+            //byte Number;
+            //Console.WriteLine("<------------Byte 0-255-------------> ");
+            //Console.Write("Byte: ");
+            //Number=Convert.ToByte(Console.ReadLine());
+            //Console.WriteLine(Number);
+            //Console.Read();
+
+
+
+            //byte s1, s2, Multiply;
+            //Console.WriteLine("Multipley= MAX= 255 , MIN= 0  . 0+0=0 // 255+0=250");
+            //Console.Write("S1: ");
+            //s1=Convert.ToByte(Console.ReadLine());
+            //Console.Write("S2: ");
+            //s2=Convert.ToByte(Console.ReadLine());
+            //Multiply = Convert.ToByte(s1+s2);
+            //Console.WriteLine(Multiply);
+            //Console.Read();
+
+
+            //Sbyte Veriables ------------------------------------------------------------------------->
+
+
+            ////Console.WriteLine("<------------Byte -128/127-------------> ");
+            //sbyte s1;
+            //s1 = 127;
+            //Console.Write(s1);
+            //Console.Read();
+
+
+            //sbyte s1, s2, multiply;
+            //Console.Write("S1: ");
+            //s1=Convert.ToSByte(Console.ReadLine());
+            //Console.Write("S2: ");
+            //s2=Convert.ToSByte(Console.ReadLine());
+            //multiply=Convert.ToSByte(s1+s2);
+            //Console.Write("Multiply");
+            //Console.WriteLine(multiply);
+            //Console.Read();
+
+
+
+            //Float Veriables ------------------------------------------------------------------------->
+            ////Console.WriteLine("<------------Float 10f -------------> ")
+
+            //float s1;
+            //s1 = 100.20f;
+            //Console.WriteLine(s1);
+            //Console.Read();
+
+
+            //float s1, s2, Multiply;
+            //Console.Write("S1: ");
+            //s1 = float.Parse(Console.ReadLine());
+            //Console.Write("S2: ");
+            //s2 = float.Parse(Console.ReadLine());
+            //Multiply = s1 + s2;
+            //Console.WriteLine(Multiply);
+            //Console.Read();
+
+            //Short Veriables ------------------------------------------------------------------------->
+            ////Console.WriteLine("<------------ Short= min= -32768  max=32767 -------------> ")
+
+            //short s1;
+            //Console.Write("S1: ");
+            //s1 = 10;
+            //Console.WriteLine(s1);
+            //Console.Read();
+
+
+            //short s1, s2, Multiply;
+            //Console.Write("S1: ");
+            //s1 = short.Parse(Console.ReadLine());
+            //Console.Write("S2: ");
+            //s2=short.Parse(Console.ReadLine());
+            //Multiply =(short)(s1+s2);
+            //Console.Write(Multiply);
+            //Console.Read();
+
+
+            //UShort Veriables ------------------------------------------------------------------------->
+            ////Console.WriteLine("<------------ UShort= min= 0 max=65535 -------------> ")
+            ///
+            //ushort s1;
+            //s1 = 25;
+            //Console.WriteLine(s1);
+            //Console.Read();
+
+
+            //ushort s1, s2, Multiply;
+            //Console.Write("S1: ");
+            //s1=ushort.Parse(Console.ReadLine());
+            //Console.Write("S2: ");
+            //s2=ushort.Parse(Console.ReadLine());
+            //Multiply=(ushort)(s1+s2);
+            //Console.WriteLine(Multiply);
+            //Console.Read();
+
+
+            //Deccimal Veriables ------------------------------------------------------------------------->
+            ////Console.WriteLine("<------------ Decimal 16byte -------------> ")
+
+            //decimal s1;
+            //s1 = 50;
+            //Console.WriteLine(s1);
+            //Console.Read();
+
+
+            //decimal area, environment,edge;
+            //Console.Write("Edge: ");
+            //edge = Convert.ToDecimal(Console.ReadLine());
+            //area = edge * edge;
+            //environment = edge * 4;
+            //Console.WriteLine("edge: " + area + "encvironment:" + environment);
+            //Console.Read();
+
+
+            //Max Min Value ------------------------------------------------------------------------->
+            //Console.WriteLine(int.MaxValue);
+            //Console.WriteLine(int.MinValue);
+            //Console.WriteLine(byte.MaxValue);
+            //Console.WriteLine(byte.MinValue);
+            //Console.WriteLine(float.MaxValue);
+            //Console.WriteLine(float.MinValue);
+            //Console.WriteLine(double.MaxValue);
+            //Console.WriteLine(double.MinValue);
+            //Console.WriteLine(sbyte.MaxValue);
+            //Console.WriteLine(sbyte.MinValue);
+
+            //Bool Veriables ------------------------------------------------------------------------->
+            ////Console.WriteLine("<------------ Bool -------------> ")
+
+            //bool situation;
+            //situation = true;
+            //Console.Write("situation: ");
+            //Console.WriteLine(situation);
+            //Console.Read();
+
+            //bool Situation;
+            //Console.Write("Situation: ");
+            //Situation = Convert.ToBoolean(Console.ReadLine());
+            //Console.Write(Situation);
+            //Console.Read();
+        }
+    }
+}
